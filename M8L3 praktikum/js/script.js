7b95c6bc46399846fb9587279debbae53182a757
MelvinChrysander <130719873+MelvinChrysander@users.noreply.github.com>
2024-09-01 22:14:35 +0700

    Update script.js
    1/09/24 menambah sidebar, navigator, tambahan carousel

--- M8L3 praktikum/js/script.js
+++ M8L3 praktikum/js/script.js
@@ -1,27 +1,35 @@
+let burger = document.querySelector('.burger-menu');
+let nav = document.querySelector('.main-nav');
+
 $('.carousel').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    prevArrow: '<img src="img/Arrow_left.svg">',
-    nextArrow: '<img src="img/Arrow_right.svg">',
-    slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
+  centerMode: true,
+  centerPadding: '60px',
+  prevArrow: '<img src="img/Arrow_left.svg">',
+  nextArrow: '<img src="img/Arrow_right.svg">',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
       }
-    ]
-  });
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+burger.addEventListener('click', function () {
+  burger.classList.toggle('active');
+  nav.classList.toggle('active');
+});
